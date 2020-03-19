53bb6540a9e6269bc3573c0152dbee99ecf6adf3
Jingxian Qiu <jingxianqiu@jingxians-mbp.home>
2020-03-20 10:53:47 +1100

    play with React Hook

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (<div>
+      Hello {this.props.name}
+      <Example />
+    </div>);
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Vickyss" />, mountNode);
+ReactDOM.render(<HelloMessage name="Vicky" />, mountNode);
